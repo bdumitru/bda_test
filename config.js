2d39bae55c69cc15ca89ee7f9fa7ca85bed9f82a
bdumitru <23194949+bdumitru@users.noreply.github.com>
2025-06-18 17:27:02 +0200

    fix s3 output folder regex

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const awsAccId = process.env.AWS_ACCOUNT_ID;
 const bdaProjectArn = process.env.BDA_PROJECT_ARN;
 const s3BucketName = process.env.BDA_S3_BUCKET;
 const awsRegion = process.env.AWS_REGION;
-const bdaOutputFolder = String(process.env.BDA_OUTPUT_FOLDER || 'bdaout').replace(/(?:^\s*\/)||(?:\/\s*$)/ig, '')
+const bdaOutputFolder = String(process.env.BDA_OUTPUT_FOLDER || 'bdaout').replace(/(?:^\s*\/|\/\s*$)/ig, '')
 
 export {
     awsAccId,
